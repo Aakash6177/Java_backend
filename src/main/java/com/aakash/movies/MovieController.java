e9d0079857ac40da494243661407829da9a5eecf
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2023-04-14 14:43:36 -0700

    changed the endpoint

--- src/main/java/com/aakash/movies/MovieController.java
+++ src/main/java/com/aakash/movies/MovieController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/movies")
+@RequestMapping("/movies")
 public class MovieController {
     @GetMapping
     public ResponseEntity<String> allMovies(){
